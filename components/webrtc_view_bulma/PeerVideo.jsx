f9bc8d1be1ffec2ef37321ae0749c2ec06533829
Mike Lippert <mike@rifflearning.com>
2019-02-03 18:13:06 -0500

    fix style issues in 3 files (reduced total issues from 4724 to 4507
    Issues that need some thought on how to best address them:
    
    incorrect header: Override because riff code should have a different header
    missing in props validation: Fix by adding Prop type validation
    Unexpected usage of doublequote in jsx code: We may prefer this, so override?
    Prop must be placed on a new line:
    Unexpected unnamed function:
    Unexpected space before function parentheses: Override because we prefer that unnamed
                                                  functions have a space before the parens
    Unexpected use of process.env: This is an actual issue that needs fixing. The values
                                   referenced should be dynamic configuration not compiled statically

--- components/webrtc_view_bulma/PeerVideo.jsx
+++ components/webrtc_view_bulma/PeerVideo.jsx
@@ -1,67 +1,70 @@
+// Copyright (c) 2018-present Riff Learning, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
 import React from 'react';
-import { logger } from '../../utils/riff';
-import * as sc from './styled';
+
+import {logger} from '../../utils/riff';
 
 class PeerVideo extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
         this.appendVideo = this.appendVideo.bind(this);
         this.video = this.props.videoEl;
     }
 
-    appendVideo (el) {
-      logger.debug("appending?", "color:", this.props.peerColor)
-      if (el !== null) {
-          this.video.style.setProperty('overflow', 'hidden');
-          this.video.style.setProperty('display', 'block');
-          this.video.style.setProperty('width', '100%');
-          this.video.style.setProperty('height', '100%');
-          this.video.style.setProperty('margins', '5px');
-          this.video.style.setProperty('border-radius', '5px');
-          if (this.props.type === "peer") {
-              // we don't want to clip any of the shared screen,
-              // so only apply this to peers
-              this.video.style.setProperty('object-fit', 'cover');
-              this.video.style.setProperty('border-bottom-right-radius', '0px');
-              this.video.style.setProperty('border-bottom-left-radius', '0px');
-          }
-          el.appendChild(this.video);
-          this.video.play()
-      }
+    appendVideo(el) {
+        logger.debug('appending?', 'color:', this.props.peerColor);
+        if (el !== null) {
+            this.video.style.setProperty('overflow', 'hidden');
+            this.video.style.setProperty('display', 'block');
+            this.video.style.setProperty('width', '100%');
+            this.video.style.setProperty('height', '100%');
+            this.video.style.setProperty('margins', '5px');
+            this.video.style.setProperty('border-radius', '5px');
+            if (this.props.type === 'peer') {
+                // we don't want to clip any of the shared screen,
+                // so only apply this to peers
+                this.video.style.setProperty('object-fit', 'cover');
+                this.video.style.setProperty('border-bottom-right-radius', '0px');
+                this.video.style.setProperty('border-bottom-left-radius', '0px');
+            }
+            el.appendChild(this.video);
+            this.video.play();
+        }
     }
 
-    render () {
-      let style = {
-          'padding': '0.25rem',
-      };
+    render() {
+        const style = {
+            padding: '0.25rem',
+        };
 
-      if (this.props.type === "peer") {
-          style.borderBottom = '5px solid ' + this.props.peerColor;
-          style.padding = '0';
-          style.margin = '0.25rem';
-          style.borderBottomRightRadius = '5px';
-          style.borderBottomLeftRadius = '5px';
-      }
+        if (this.props.type === 'peer') {
+            style.borderBottom = '5px solid ' + this.props.peerColor;
+            style.padding = '0';
+            style.margin = '0.25rem';
+            style.borderBottomRightRadius = '5px';
+            style.borderBottomLeftRadius = '5px';
+        }
 
-      let classes = "videoContainer remotes column";
+        let classes = 'videoContainer remotes column';
 
-      if (this.props.peerLength < 4) {
-          style.width = '100vh';
-          style.height = '80vh';
-      } else {
-          style.width = '50vh';
-          style.height = '40vh';
-          classes += " is-narrow";
-      }
+        if (this.props.peerLength < 4) {
+            style.width = '100vh';
+            style.height = '80vh';
+        } else {
+            style.width = '50vh';
+            style.height = '40vh';
+            classes += ' is-narrow';
+        }
 
-      return (
-        <div
-            className={classes}
-            id={"container_" + this.props.id}
-            style={style}
-            ref={this.appendVideo}
-        />
-      );
+        return (
+            <div
+                className={classes}
+                id={'container_' + this.props.id}
+                style={style}
+                ref={this.appendVideo}
+            />
+        );
     }
 }
 
--- utils/webrtc/webrtc.js
+++ utils/webrtc/webrtc.js
@@ -1,140 +1,139 @@
+// Copyright (c) 2018-present Riff Learning, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
 import SimpleWebRtc from 'simplewebrtc';
-
-import sibilant from 'sibilant-webaudio';
-
+import Sibilant from 'sibilant-webaudio';
 import parse from 'url-parse';
 
 import * as WebRtcActions from '../../actions/webrtc_actions';
-
-import {app, logger} from '../riff';
-
 import {updateRiffMeetingId} from '../../actions/views/riff';
+import {app, logger} from '../riff';
 
 export const createWebRtcLink = (teamName, channelName) => {
     const link = parse(window.location.href, true);
-    link.set('pathname', teamName + '/' + channelName + '/' + 'video' + '/' + generateUID());
-    console.log("Created webrtc Link:", link.href);
+    link.set('pathname', `${teamName}/${channelName}/video/${generateUID()}`);
+    logger.debug('Created webrtc Link:', link.href);
     return link;
-}
+};
 
 function generateUID() {
     // I generate the UID from two parts here
     // to ensure the random number provide enough bits.
     var firstPart = (Math.random() * 46656) | 0;
     var secondPart = (Math.random() * 46656) | 0;
-    firstPart = ("000" + firstPart.toString(36)).slice(-3);
-    secondPart = ("000" + secondPart.toString(36)).slice(-3);
+    firstPart = ('000' + firstPart.toString(36)).slice(-3);
+    secondPart = ('000' + secondPart.toString(36)).slice(-3);
     return firstPart + secondPart;
 }
-
 
 export function isScreenShareSourceAvailable() {
     // currently we only support chrome v70+ (w/ experimental features enabled, if necessary)
     // and firefox
     return (navigator.getDisplayMedia ||
         navigator.mediaDevices.getDisplayMedia ||
-        !!navigator.mediaDevices.getSupportedConstraints().mediaSource);
+        Boolean(navigator.mediaDevices.getSupportedConstraints().mediaSource));
 }
 
 export default function (localVideoNode, dispatch, getState) {
     //TODO: make dynamic
     let signalmasterPath = process.env.CLIENT_ENV.SIGNALMASTER_PATH || '';
     signalmasterPath += '/socket.io';
-    let signalmasterUrl = process.env.CLIENT_ENV.SIGNALMASTER_URL;
+    const signalmasterUrl = process.env.CLIENT_ENV.SIGNALMASTER_URL;
     const webRtcConfig = {
         localVideoEl: localVideoNode,
-        remoteVideosEl: "",
+        remoteVideosEl: '',
         autoRequestMedia: true,
         url: signalmasterUrl,
         socketio: {
             path: signalmasterPath,
-            forceNew: true
+            forceNew: true,
         },
         media: {
             audio: true,
             video: {
                 width: {ideal: 640},
                 height: {ideal: 480},
-                frameRate: {max: 30}
-            }
+                frameRate: {max: 30},
+            },
         },
-        debug: true
+        debug: true,
     };
 
     const webrtc = new SimpleWebRtc(webRtcConfig);
 
-
-    webrtc.on('videoAdded', function (video, peer) {
-        console.log("added video", video, peer);
+    webrtc.on('videoAdded', (video, peer) => {
+        logger.debug('added video', video, peer);
         dispatch(WebRtcActions.addPeer({peer}));
     });
 
-    webrtc.on('videoRemoved', function (video, peer) {
+    webrtc.on('videoRemoved', (video, peer) => {
         const state = getState();
+
         // get riffId
         if (state.views.webrtc.inRoom) {
-            dispatch(WebRtcActions.removePeer({peer: peer,
-                                               videoEl: video}));
-            const [riffId, ...rest] = peer.nick.split("|");
+            dispatch(WebRtcActions.removePeer({peer,
+                                               videoEl: video})); // eslint-disable-line indent
+            const [riffId, ...rest] = peer.nick.split('|'); // eslint-disable-line no-unused-vars
+
             //TODO: state get here is wrong.
             dispatch(WebRtcActions.riffParticipantLeaveRoom(state.views.riff.meetingId, riffId));
         }
     });
 
-    webrtc.on('screenAdded', function (video, peer) {
-        logger.debug("adding shared screen!", video, "from", peer);
-        dispatch(WebRtcActions.addSharedScreen({ videoEl: video, peer: peer }));
+    webrtc.on('screenAdded', (video, peer) => {
+        logger.debug('adding shared screen!', video, 'from', peer);
+        dispatch(WebRtcActions.addSharedScreen({videoEl: video, peer}));
     });
 
-    webrtc.on('screenRemoved', function (video, peer) {
-        logger.debug("removing shared screen!", video);
+    webrtc.on('screenRemoved', (video) => {
+        logger.debug('removing shared screen!', video);
         dispatch(WebRtcActions.removeSharedScreen());
     });
 
-    webrtc.on('localScreenAdded', function (video) {
+    webrtc.on('localScreenAdded', (video) => {
         dispatch(WebRtcActions.addLocalSharedScreen(video));
     });
 
-    webrtc.on('localScreenRemoved', function (video) {
+    webrtc.on('localScreenRemoved', (video) => {
         dispatch(WebRtcActions.removeLocalSharedScreen(video));
     });
 
     // this happens if the user ends via the chrome button
     // instead of our button
-    webrtc.on('localScreenStopped', function (video) {
+    webrtc.on('localScreenStopped', (video) => {
         dispatch(WebRtcActions.removeLocalSharedScreen(video));
     });
 
-    webrtc.on('localScreenRequestFailed', function () {
+    webrtc.on('localScreenRequestFailed', () => {
         dispatch(WebRtcActions.getDisplayError());
     });
 
-    webrtc.on('localStreamRequestFailed', function (event) {
+    webrtc.on('localStreamRequestFailed', (event) => {
         dispatch(WebRtcActions.getMediaError(event));
     });
 
-    webrtc.on('localStream', function (stream) {
+    webrtc.on('localStream', (stream) => {
         if (stream.active) {
             dispatch(WebRtcActions.getMediaSuccess());
         }
     });
 
-    webrtc.on('readyToCall', function (video, peer) {
-        let stream = webrtc.webrtc.localStreams[0];
+    webrtc.on('readyToCall', (/*video, peer*/) => {
+        const stream = webrtc.webrtc.localStreams[0];
         dispatch(WebRtcActions.getMediaSuccess());
-        var sib = new sibilant(stream);
+        const sib = new Sibilant(stream);
         if (sib) {
-            webrtc.stopVolumeCollection = function () {
+            webrtc.stopVolumeCollection = () => {
                 // sib.unbind('volumeChange');
             };
 
-            webrtc.startVolumeCollection = function () {
-                sib.bind('volumeChange', function (data) {
+            webrtc.startVolumeCollection = () => {
+                sib.bind('volumeChange', (data) => {
                     const state = getState();
                     if (!state.views.webrtc.inRoom) {
                         dispatch(WebRtcActions.volumeChanged(data));
                     }
-                }.bind(getState));
+                });
             };
 
             webrtc.startVolumeCollection();
@@ -146,16 +145,15 @@ export default function (localVideoNode, dispatch, getState) {
                 room: getState().views.webrtc.roomName,
                 startTime: data.start.toISOString(),
                 endTime: data.end.toISOString(),
-                token: getState().views.riff.authToken
-            }).then(function (res) {
+                token: getState().views.riff.authToken,
+            }).then((res) => {
                 dispatch(updateRiffMeetingId(res.meeting));
-            }).catch(function (err) {
+            }).catch((err) => { // eslint-disable-line no-unused-vars
                 // error thrown, throw it?
             });
         });
 
-
-        webrtc.stopSibilant = function () {
+        webrtc.stopSibilant = () => {
             sib.unbind('volumeChange');
             sib.unbind('stoppedSpeaking');
         };
@@ -170,4 +168,5 @@ export default function (localVideoNode, dispatch, getState) {
     };
 
     return webrtc;
-};
+}
+
