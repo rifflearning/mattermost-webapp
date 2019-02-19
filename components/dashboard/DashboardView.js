677b945c1174e69016e77a3c6770cc599dbad7ce
Mike Lippert <mike@rifflearning.com>
2019-02-19 14:29:51 -0500

    fix many style errors in DashboardView.js

--- components/dashboard/DashboardView.js
+++ components/dashboard/DashboardView.js
@@ -1,36 +1,18 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// Copyright (c) 2018-present Riff Learning, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
+
+/* eslint
+    header/header: "off",
+ */
+
 import React from 'react';
 import styled from 'styled-components';
 import {ScaleLoader} from 'react-spinners';
-import MaterialIcon from 'material-icons-react';
 import _ from 'underscore';
 
+import {logger} from 'utils/riff';
+
 import MeetingViz from './components/MeetingViz';
-
-const SpaceBetweeen = styled.div.attrs({
-    className: 'space-between',
-})`
-    display: flex;
-    justify-content: space-between;
-    width: 320px;
-`;
-
-const MeetingTabs = styled.div.attrs({
-    className: 'timeline',
-})`
-    padding-left: 2.5rem;
-    overflow-y: scroll;
-    max-height: 100%;
-    &::-webkit-scrollbar {
-        width: 0px; /* remove scrollbar space */
-        background: transparent; /* optional: just make scrollbar invisible */
-    }
-    /* optional: show position indicator in red */
-    &::-webkit-scrollbar-thumb {
-        background: #ff0000;
-    }
-`;
 
 const NoMeetingsMessage = styled.div.attrs({
     className: 'no-meetings-message',
@@ -49,66 +31,66 @@ const NoMeetingsMessage = styled.div.attrs({
 // All dispatches were ripped out re-copy back in once server is working
 
 const DashboardView = (props) => {
-    console.log("dashboard view props:", props);
+    logger.debug('dashboard view props:', props);
     if (props.loadingStatus) {
         return (
             <div className='columns is-centered has-text-centered'>
-              <div className='column'>
-                <ScaleLoader color={'#8A6A94'}/>
-              </div>
+                <div className='column'>
+                    <ScaleLoader color={'#8A6A94'}/>
+                </div>
             </div>
         );
     } else if (props.loadingError.status) {
         return (
             <div
-              className='columns is-centered has-text-centered is-vcentered'
-              style={{height: '92vh'}}
-              >
-              <div
-                className='column is-vcentered'
-                style={{alignItems: 'center'}}
+                className='columns is-centered has-text-centered is-vcentered'
+                style={{height: '92vh'}}
+            >
+                <div
+                    className='column is-vcentered'
+                    style={{alignItems: 'center'}}
                 >
-                <p className='is-size-4 is-primary'>
-                  {props.loadingError.message}
-                </p>
-                <ScaleLoader color={'#8A6A94'}/>
-              </div>
+                    <p className='is-size-4 is-primary'>
+                        {props.loadingError.message}
+                    </p>
+                    <ScaleLoader color={'#8A6A94'}/>
+                </div>
             </div>
         );
     }
+
     // marginLeft on column is a quick fix until we fix the styling on this ugly page.
-    console.log("only loading:", props.numLoadedMeetings, 'Meetings');
+    logger.debug('only loading:', props.numLoadedMeetings, 'Meetings');
     const meetingVisualizations = _.map(_.first(props.meetings, props.numLoadedMeetings), (m) => {
         return (
-            <MeetingViz key={m._id}
-                        meeting={m}
-                        allMeetings={props.meetings}
-                        maybeLoadNextMeeting={props.maybeLoadNextMeeting}
-                        user={props.user}/>
-        )
+            <MeetingViz
+                key={m._id} // eslint-disable-line no-underscore-dangle
+                meeting={m}
+                allMeetings={props.meetings}
+                maybeLoadNextMeeting={props.maybeLoadNextMeeting}
+                user={props.user}
+            />
+        );
     });
     return (
-        <div
-          className='app__content'
-          >
-          {props.statsStatus === 'loading' ? (
-              <div>
-                <ScaleLoader color={'#8pA6A94'}/>
-              </div>
-          ) : (
-              <div style={{overflowY: 'scroll'}}>
-                {props.meetings.length > 0 ?
-                  meetingVisualizations
-                  :
-                  <NoMeetingsMessage>
-                      <div>Welcome to your Riff Dashboard!</div>
-                      <br />
-                      <div>Once you have a Riff video meeting,</div>
-                      <div>your Riff Stats will display here.</div>
-                  </NoMeetingsMessage>
-                }
-              </div>
-          )}
+        <div className='app__content'>
+            {props.statsStatus === 'loading' ? (
+                <div>
+                    <ScaleLoader color={'#8pA6A94'}/>
+                </div>
+            ) : (
+                <div style={{overflowY: 'scroll'}}>
+                    {props.meetings.length > 0 ?
+                        meetingVisualizations :
+                        <NoMeetingsMessage>
+                            <div>{'Welcome to your Riff Dashboard!'}</div>
+                            <br/>
+                            <div>{'Once you have a Riff video meeting,'}</div>
+                            <div>{'your Riff Stats will display here.'}</div>
+                        </NoMeetingsMessage>
+                    }
+                </div>
+            )}
         </div>
     );
 };
