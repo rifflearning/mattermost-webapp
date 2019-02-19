e930c29116717438a6ad25baac1b6d20a95b28c0
Mike Lippert <mike@rifflearning.com>
2019-02-19 17:28:27 -0500

    fixed more style errors

--- components/webrtc_view_bulma/SharedScreen.jsx
+++ components/webrtc_view_bulma/SharedScreen.jsx
@@ -1,32 +1,37 @@
+// Copyright (c) 2018-present Riff Learning, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+/* eslint
+    header/header: "off",
+ */
+
 import React from 'react';
 
 import PeerVideo from './PeerVideo';
-
 import PeerAudio from './PeerAudio';
 
 class SharedScreen extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     isolateAudioFromPeer(peer) {
         const audioTrack = peer.videoEl.srcObject.getAudioTracks()[0];
         const stream = new MediaStream();
         stream.addTrack(audioTrack);
-        return <PeerAudio audio={stream}
-                    id={peer.id} />;
+        return (
+            <PeerAudio
+                audio={stream}
+                id={peer.id}
+            />
+        );
     }
 
     peerAudioTracks() {
         return this.props.peers.map((peer) => {
             // don't add audio from shared screen
             // this would cause echo / reverb
-            if (!this.props.videoEl.id.includes(peer.id)) {
-              return this.isolateAudioFromPeer(peer);
+            if (this.props.videoEl.id.includes(peer.id)) {
+                return false;
             }
-            else {
-              return false;
-            }
+
+            return this.isolateAudioFromPeer(peer);
         });
     }
 
@@ -34,9 +39,9 @@ class SharedScreen extends React.Component {
         return (
             <React.Fragment>
                 <PeerVideo
-                    key="shared_screen"
-                    id="shared_screen"
-                    type="screen"
+                    key='shared_screen'
+                    id='shared_screen'
+                    type='screen'
                     videoEl={this.props.videoEl}
                     peerLength={1}
                 />
