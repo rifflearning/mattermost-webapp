fed1029d8d13f192d8ed407e35918f6ab3b3bb72
Brec <lebrecdesign@gmail.com>
2019-02-19 14:36:09 -0500

    Fixed check-style errors.

--- components/webrtc_view_bulma/SharedScreen.js
+++ components/webrtc_view_bulma/SharedScreen.js
@@ -1,44 +1,49 @@
 import React from 'react';
-import { logger } from '../../utils/riff';
+
 import PeerVideo from './PeerVideo';
+
 import PeerAudio from './PeerAudio';
 
 class SharedScreen extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  isolateAudioFromPeer(peer) {
-    let audioTrack = peer.videoEl.srcObject.getAudioTracks()[0];
-    let stream = new MediaStream();
-    stream.addTrack(audioTrack);
-    return <PeerAudio audio={stream} id={peer.id} />
-  }
+    isolateAudioFromPeer(peer) {
+        const audioTrack = peer.videoEl.srcObject.getAudioTracks()[0];
+        const stream = new MediaStream();
+        stream.addTrack(audioTrack);
+        return <PeerAudio audio={stream}
+                    id={peer.id} />;
+    }
 
-  peerAudioTracks() {
-    return this.props.peers.map((peer) => {
-        // don't add audio from shared screen
-        // this would cause echo / reverb
-        if (!this.props.videoEl.id.includes(peer.id)) {
-          return this.isolateAudioFromPeer(peer)
-        }
-    });
-  }
+    peerAudioTracks() {
+        return this.props.peers.map((peer) => {
+            // don't add audio from shared screen
+            // this would cause echo / reverb
+            if (!this.props.videoEl.id.includes(peer.id)) {
+              return this.isolateAudioFromPeer(peer);
+            }
+            else {
+              return false;
+            }
+        });
+    }
 
-  render() {
-    return (
-      <React.Fragment>
-        <PeerVideo
-          key="shared_screen"
-          id="shared_screen"
-          type="screen"
-          videoEl={this.props.videoEl}
-          peerLength={1}
-        />
-        {this.peerAudioTracks()}
-      </React.Fragment>
-    );
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <PeerVideo
+                    key="shared_screen"
+                    id="shared_screen"
+                    type="screen"
+                    videoEl={this.props.videoEl}
+                    peerLength={1}
+                />
+                {this.peerAudioTracks()}
+            </React.Fragment>
+        );
+    }
 }
 
 export default SharedScreen;
