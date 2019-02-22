5145467c7b01b95cf144e25a0710e1b74b227e5f
Mike Lippert <mike@rifflearning.com>
2019-02-22 14:44:53 -0500

    fix style errors (-> 2712)

--- components/dashboard/components/TurnChart.jsx
+++ components/dashboard/components/TurnChart.jsx
@@ -1,35 +1,37 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// Copyright (c) 2018-present Riff Learning, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import React, {Component} from 'react';
-import {withRouter} from 'react-router-dom';
-import styled, {injectGlobal, keyframes} from 'styled-components';
-import {Link} from 'react-router-dom';
-import ReactChartkick, {ColumnChart, PieChart} from 'react-chartkick';
+
+/* eslint
+    header/header: "off",
+    dot-location: ["error", "property"],
+    indent: ["error", 4, { "CallExpression": { "arguments": "first" } }],
+ */
+
+import React from 'react';
+import ReactChartkick, {PieChart} from 'react-chartkick';
 import {ScaleLoader} from 'react-spinners';
-import MaterialIcon from 'material-icons-react';
 import Chart from 'chart.js';
-import ChartTable from './ChartTable';
 import _ from 'underscore';
 
-import moment from 'moment';
+import {logger} from 'utils/riff';
 
+import ChartTable from './ChartTable';
 import ChartCard from './ChartCard';
 
 ReactChartkick.addAdapter(Chart);
 
 const formatChartData = (processedUtterances, participantId) => {
-    console.log('formatting:', processedUtterances);
+    logger.debug('formatting:', processedUtterances);
 
     const colorYou = '#ab45ab';
-    const colorOther = '#bdc3c7';
     let nextOtherUser = 1;
 
     const data = [];
     const peerColors = ['#f56b6b', '#128EAD', '#7caf5f', '#f2a466'];
     const colors = [];
-    processedUtterances = _.sortBy(processedUtterances, 'participantId');
+    const sortedUtterances = _.sortBy(processedUtterances, 'participantId');
 
-    processedUtterances.forEach((p) => {
+    sortedUtterances.forEach((p) => {
         // our display name from firebase if we've got it.
         const label = p.name;
 
@@ -51,13 +53,11 @@ equal speaking time across all members is associated with higher creativity, mor
 group members, and better brainstorming.';
 
 const TurnChart = ({processedUtterances, participantId, loaded}) => {
-
-
-    var chartDiv;
-    var chartTable;
+    let chartDiv;
+    let chartTable;
     if (loaded) {
         const r = formatChartData(processedUtterances, participantId);
-        console.log('data for chart:', r.data);
+        logger.debug('data for chart:', r.data);
 
         const chartOptions = {
             tooltips: {
@@ -65,15 +65,11 @@ const TurnChart = ({processedUtterances, participantId, loaded}) => {
                     label(tooltipItem, data) {
                         const label = data.labels[tooltipItem.index] || '';
                         let seconds =
-                            data.datasets[tooltipItem.datasetIndex].data[
-                                tooltipItem.index
-                            ] || -1;
+                            data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index] || -1;
                         const minutes = Math.trunc(seconds / 60);
                         seconds = Math.round(seconds % 60);
 
-                        const tooltip = `${label}${
-                        label ? ': ' : ''
-                    }${minutes}m ${seconds}s`;
+                        const tooltip = `${label}${label ? ': ' : ''}${minutes}m ${seconds}s`;
                         return tooltip;
                     },
                 },
@@ -82,34 +78,34 @@ const TurnChart = ({processedUtterances, participantId, loaded}) => {
 
         chartDiv = (
             <PieChart
-              donut={true}
-              library={chartOptions}
-              data={r.data}
-              colors={r.colors}
-              height='250px'
-              width='100%'
-              />
+                donut={true}
+                library={chartOptions}
+                data={r.data}
+                colors={r.colors}
+                height='250px'
+                width='100%'
+            />
         );
 
         const totalTime = r.data.reduce((prev, curr) => {
-          return prev + curr[1];
+            return prev + curr[1];
         }, 0);
 
         const getTimeString =
-          seconds => `${Math.trunc(seconds / 60)} minutes ${Math.round(seconds % 60)} seconds`;
+            (seconds) => `${Math.trunc(seconds / 60)} minutes ${Math.round(seconds % 60)} seconds`;
 
         const getPercentageString =
-          seconds => `${Math.round((seconds / totalTime) * 100)}%`;
+            (seconds) => `${Math.round((seconds / totalTime) * 100)}%`;
 
         chartTable = (
-         <ChartTable
-           cols={['Participant', 'Percent Speaking Time', 'Total Speaking Time']}
-           rows={r ? r.data.map(participant => [
-             participant[0],
-             getPercentageString(participant[1]),
-             getTimeString(participant[1])
-           ]) : []}
-         />
+            <ChartTable
+                cols={['Participant', 'Percent Speaking Time', 'Total Speaking Time']}
+                rows={r ? r.data.map((participant) => [
+                    participant[0],
+                    getPercentageString(participant[1]),
+                    getTimeString(participant[1]),
+                ]) : []}
+            />
         );
     }
 
@@ -119,11 +115,11 @@ const TurnChart = ({processedUtterances, participantId, loaded}) => {
 
     return (
         <ChartCard
-          title='Speaking Time'
-          chartDiv={loaded ? chartDiv : loadingDiv}
-          chartInfo={chartInfo}
-          maxWidth={100}
-          chartTable={loaded ? chartTable : false}
+            title='Speaking Time'
+            chartDiv={loaded ? chartDiv : loadingDiv}
+            chartInfo={chartInfo}
+            maxWidth={100}
+            chartTable={loaded ? chartTable : false}
         />
     );
 };
