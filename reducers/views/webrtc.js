e0a6467b304d727fe20d8daf9026bdbda30b7afa
Mike Lippert <mike@rifflearning.com>
2019-03-20 14:48:48 -0400

    Fix check-style errors in reducers/views/webrtc.js

--- reducers/views/webrtc.js
+++ reducers/views/webrtc.js
@@ -1,8 +1,28 @@
-import {joinWebRtcRoom} from '../../actions/webrtc_actions';
-import {WebRtcActionTypes, ActionTypes} from '../../utils/constants';
+// Copyright (c) 2018-present Riff Learning, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+/* eslint
+    header/header: "off",
+    dot-location: ["error", "property"],
+    indent: ["error", 4, { "CallExpression": { "arguments": "first" } }],
+    "react/jsx-max-props-per-line": ["error", { "when": "multiline" }],
+    "no-underscore-dangle": ["error", { "allow": [ "_id" ] }],
+ */
+
+import {logger, Colors} from 'utils/riff';
+import {WebRtcActionTypes, ActionTypes} from 'utils/constants';
+import {joinWebRtcRoom} from 'actions/webrtc_actions';
 
 const initialState = {
-    peerColors: ['#f56b6b', '#128EAD', '#7caf5f', '#f2a466', '#321325', '#3C493F', '#1B998B'],
+    peerColors: [
+        Colors.reddishPeach,
+        Colors.turkoise,
+        Colors.sage,
+        Colors.apricot,
+        Colors.eggplant,
+        Colors.darkfern,
+        Colors.aquamarine,
+    ],
     getMediaStatus: 'error',
     getMediaMessage: '',
 
@@ -24,19 +44,17 @@ const initialState = {
 
     textchat: {
         messages: [],
-        lastRoom: "",
-        lastMeetingId: "",
-        badge: 0
+        lastRoom: '',
+        lastMeetingId: '',
+        badge: 0,
     },
     userSharing: false,
     webRtcLocalSharedScreen: null,
-    webRtcRemoteSharedScreen: null
+    webRtcRemoteSharedScreen: null,
 };
 
-
-
 const webrtc = (state = initialState, action) => {
-    switch(action.type) {
+    switch (action.type) {
     case (WebRtcActionTypes.JOIN_ROOM):
         return {...state,
             joiningRoom: true,
@@ -52,47 +70,46 @@ const webrtc = (state = initialState, action) => {
             shouldFocusJoinRoomError: action.status === 'error',
         };
 
-    case (WebRtcActionTypes.ADD_PEER):
+    case (WebRtcActionTypes.ADD_PEER): {
         // this removes any null peers
-        console.log('adding peer', action);
+        logger.debug('adding peer', action);
         const [riffId, displayName] = action.peer.peer.nick.split('|');
-        console.log('adding peer', riffId, displayName);
+        logger.debug('adding peer', riffId, displayName);
         const peers = state.webRtcPeers.filter((n) => !(n === null));
         const peerIds = state.webRtcPeers.map((p) => p.id);
         if (peerIds.indexOf(action.peer.id) >= 1) {
-            console.log('not re-adding a peer...');
+            logger.debug('not re-adding a peer...');
             return state;
-        } else {
-            // basic object destructuring to get a simple subset of our peer object.
-            const newPeer = (({id, nick, type, videoEl}) => ({id, nick, type, videoEl}))(action.peer.peer);
-            const allPeers = [...peers, newPeer];
-            const displayNames = allPeers.map((p) => { return p.nick.split('|')[1];});
-            let riffIds = allPeers.map((p) => { return p.nick.split('|')[0]});
-            console.log("returning new peer", state, allPeers, displayNames, riffIds);
-            return {...state,
-                webRtcPeers: allPeers,
-                webRtcPeerDisplayNames: displayNames,
-                webRtcRiffIds: riffIds,
-            };
         }
 
-    case (WebRtcActionTypes.REMOVE_PEER):
-        let peer = action.peer.peer;
-        const index = state.webRtcPeers.map(item => item.id).indexOf(peer.id);
-        let allPeers = [...state.webRtcPeers.slice(0, index),
-                        ...state.webRtcPeers.slice(index + 1)];
-        const displayNames = allPeers.map((p) => {
-            return p.nick.split('|')[1];
-        });
-        const riffIds = allPeers.map((p) => { return p.nick.split('|')[0]; });
+        // basic object destructuring to get a simple subset of our peer object.
+        const newPeer = (({id, nick, type, videoEl}) => ({id, nick, type, videoEl}))(action.peer.peer);
+        const allPeers = [...peers, newPeer];
+        const {displayNames, riffIds} = getPeerNamesAndIds(allPeers);
+        logger.debug('returning new peer', state, allPeers, displayNames, riffIds);
         return {...state,
             webRtcPeers: allPeers,
             webRtcPeerDisplayNames: displayNames,
             webRtcRiffIds: riffIds,
         };
+    }
+
+    case (WebRtcActionTypes.REMOVE_PEER): {
+        const peerToRemove = action.peer.peer;
+        const index = state.webRtcPeers.findIndex((peer) => peer.id === peerToRemove.id);
+        const allPeers = state.webRtcPeers.slice(); // copy array
+        allPeers.splice(index, 1); // remove peer
+        const {displayNames, riffIds} = getPeerNamesAndIds(allPeers);
+        return {...state,
+            webRtcPeers: allPeers,
+            webRtcPeerDisplayNames: displayNames,
+            webRtcRiffIds: riffIds,
+        };
+    }
 
     case (WebRtcActionTypes.READY_TO_CALL):
-        return {...state, readyToCall: true,
+        return {...state,
+            readyToCall: true,
             getMediaError: false,
         };
 
@@ -128,33 +145,36 @@ const webrtc = (state = initialState, action) => {
 
     case (WebRtcActionTypes.VOLUME_CHANGED):
         if (action.volume !== null) {
-            let vol1 = (((120 - Math.abs(action.volume)) / 120)*100);
-            let vol2 = (Math.ceil(vol1)/20)*20;
+            const vol1 = (((120 - Math.abs(action.volume)) / 120) * 100);
+            const vol2 = (Math.ceil(vol1) / 20) * 20; // TODO: this divide by 20, multiply by 20 doesn't do anything does it? -mjl
             if (vol2 > 0) {
                 return {...state, volume: vol2};
-            } else {
-                return state;
             }
         }
 
-    case (ActionTypes.CLICK_VIDEO):
-        console.log("CLICK VIDEO action in webrtc reducer.")
-        joinWebRtcRoom(action.roomName, action.team_id);
+        return state; // TODO: I moved the return from inside the if to prevent fall through, not sure if it is correct though. -mjl
 
-    case (WebRtcActionTypes.TEXT_CHAT_MSG_UPDATE):
+    case (ActionTypes.CLICK_VIDEO):
+        logger.debug('CLICK VIDEO action in webrtc reducer.');
+        joinWebRtcRoom(action.roomName, action.team_id);
+        return state; // TODO: I added this to prevent fall through, not sure if it is correct though. -mjl
+
+    case (WebRtcActionTypes.TEXT_CHAT_MSG_UPDATE): {
         // will never be a message this user has sent (will always be peer)
-        console.log("text chat message update:", action)
-        let dNames = state.webRtcPeers.map((p) => { return p.nick.split("|")[1]; });
-        let rIds = state.webRtcPeers.map((p) => { return p.nick.split("|")[0]; });
-        const peerIdx = rIds.indexOf(action.messageObj.participant);
-        const dispName = "@" + dNames[peerIdx];
-        let msg = {...action.messageObj,
-                   name: dispName};
-        return {...state, textchat: {...state.textchat,
-                                     messages: [...state.textchat.messages,
-                                                msg]}};
+        logger.debug('text chat message update:', action);
+        const {displayNames, riffIds} = getPeerNamesAndIds(state.webRtcPeers);
+        const peerIdx = riffIds.indexOf(action.messageObj.participant);
+        const dispName = '@' + displayNames[peerIdx];
+        const msg = {...action.messageObj, name: dispName};
+        return {...state,
+            textchat: {...state.textchat,
+                messages: [...state.textchat.messages, msg],
+            },
+        };
+    }
+
     case (WebRtcActionTypes.TEXT_CHAT_SET_BADGE):
-        return { ...state,
+        return {...state,
             textchat: {...state.textchat,
                 badge: action.badgeValue},
         };
@@ -198,19 +218,39 @@ const webrtc = (state = initialState, action) => {
             webRtcLocalSharedScreen: action.screen,
             userSharing: true,
         };
+
     case (WebRtcActionTypes.REMOVE_LOCAL_SHARED_SCREEN):
         return {
             ...state,
             webRtcLocalSharedScreen: null,
             userSharing: false,
         };
+
     case (WebRtcActionTypes.FOCUS_JOIN_ROOM_ERROR_COMPLETE):
         return {...state,
             shouldFocusJoinRoomError: false,
         };
+
     default:
         return state;
     }
 };
+
+/**
+ * get the list of display names and the list of riff ids from
+ * a list of peers.
+ *
+ * @example
+ * let {displayNames, riffIds} = getPeerNamesAndIds(peers);
+ */
+function getPeerNamesAndIds(peers) {
+    const retObj = {displayNames: [], riffIds: []};
+    for (const peer of peers) {
+        const idAndName = peer.nick.split('|');
+        retObj.riffIds.push(idAndName[0]);
+        retObj.displayNames.push(idAndName[1]);
+    }
+    return retObj;
+}
 
 export default webrtc;
