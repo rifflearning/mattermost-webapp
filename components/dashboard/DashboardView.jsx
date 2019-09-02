db39889c0fc5c007c8f78a769c393a235721cfb9
Mike Lippert <mike@rifflearning.com>
2019-09-02 10:58:42 -0400

    Refactor the DashboardView to use a component class (and remove some unused code)
    No other functionality was modified.

--- components/dashboard/DashboardView.jsx
+++ components/dashboard/DashboardView.jsx
@@ -2,8 +2,11 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import $ from 'jquery';
 
+import * as UserAgent from 'utils/user_agent.jsx';
 import {logger} from 'utils/riff';
 
 import MeetingViz from './components/MeetingViz';
@@ -16,7 +19,7 @@ const LoadingErrorMessage = styled.div.attrs({
     left: 50%;
     transform: translate(-50%,-50%);
     text-align: center;
-    color: #4A4A4A;
+    color: #4a4a4a;
     font-size: 28px;
 `;
 
@@ -24,50 +27,147 @@ const LoadingErrorMessage = styled.div.attrs({
 
 // All dispatches were ripped out re-copy back in once server is working
 
-const DashboardView = (props) => {
-    logger.debug('only loading:', props.numMeetingsToDisplay, 'Meetings');
-    const meetingVisualizations = props.meetings.slice(0, props.numMeetingsToDisplay).map((m) => {
-        return (
-            <MeetingViz
-                key={m._id}
-                meeting={m}
-                allMeetings={props.meetings}
-                maybeLoadNextMeeting={props.maybeLoadNextMeeting}
-                user={props.user}
-            />
-        );
-    });
-    const component = () => {
-        //errored (catch all static message is shown, these errors are all related to having no meetings)
-        if (props.loadingError.status) {
-            return (
-                <LoadingErrorMessage>
-                    <div>{'Welcome to your Riff Dashboard!'}</div>
-                    <br/>
-                    <div>{'Once you have a Riff video meeting,'}</div>
-                    <div>{'your Riff stats will display here.'}</div>
-                </LoadingErrorMessage>
-            );
-        }
+/* ******************************************************************************
+ * DashboardView                                                           */ /**
+ *
+ * React component to present the Riff Metrics for meetings the user attended
+ *
+ ********************************************************************************/
+class DashboardView extends React.Component {
+    static propTypes = {
 
-        //meetings
-        if (props.meetings.length > 0) {
-            return (
-                <div style={{overflowY: 'scroll'}}>
-                    {meetingVisualizations}
-                </div>
-            );
-        }
+        /** riffdata authorization token */
+        riffAuthToken: PropTypes.string,
 
-        //default
-        return false;
+        /** The current mattermost user */
+        user: PropTypes.object.isRequired,
+
+        /** The list of meeting objects to be presented */
+        meetings: PropTypes.arrayOf(PropTypes.object).isRequired,
+
+        /** The number of meetings to display in the scrolling list */
+        numMeetingsToDisplay: PropTypes.number.isRequired,
+
+        /** ??? I'm not sure what the value signifies -mjl 2019-09-02 */
+        shouldFetch: PropTypes.bool.isRequired,
+
+        /** ??? */
+        loadingError: PropTypes.object.isRequired,
+
+        /** function to set up the connection to the riffdata server if it doesn't exist yet */
+        authenticateRiff: PropTypes.func.isRequired,
+
+        /** function to load the meetings for a particular user (it had better be the current user) */
+        loadRecentMeetings: PropTypes.func.isRequired,
+
+        /** ??? */
+        maybeLoadNextMeeting: PropTypes.func.isRequired,
     };
 
-    return (
-        <div className='app__content'>
-            {component()}
-        </div>
-    );
-};
+    /* **************************************************************************
+     * componentDidMount                                                   */ /**
+     *
+     * Lifecycle method of a React component.
+     * This is invoked immediately after a component is mounted (inserted into the
+     * tree). Initialization that requires DOM nodes should go here.
+     *
+     * Load the user's list of meetings. As this may be a time consuming operation
+     * we wait until this page is mounted and we know that the list is actually
+     * needed.
+     *
+     * @see {@link https://reactjs.org/docs/react-component.html#componentdidmount|React.Component.componentDidMount}
+     */
+    componentDidMount() {
+        if (this.props.riffAuthToken) {
+            logger.debug(`DashboardView.didMount: going to load recent meetings for ${this.props.user.id}`);
+            this.props.loadRecentMeetings(this.props.user.id);
+        }
 
-export default DashboardView;
+        $('body').addClass('app__body');
+
+        // IE Detection
+        if (UserAgent.isInternetExplorer() || UserAgent.isEdge()) {
+            $('body').addClass('browser--IE');
+        }
+    }
+
+    componentDidUpdate() {
+        if (this.props.riffAuthToken && this.props.shouldFetch) {
+            this.props.loadRecentMeetings(this.props.user.id);
+        }
+    }
+
+    /**
+     * TODO: remove this functionality from here. I'm not sure why it has to be here anyway -mjl 2019-09-02
+     */
+    UNSAFE_componentWillMount() { // eslint-disable-line camelcase
+        logger.debug('DashboardView.willMount: user:', this.props.user);
+        if (!this.props.riffAuthToken) {
+            this.props.authenticateRiff();
+        }
+    }
+
+    // componentWillUnmount() {
+    //     $('body').removeClass('app__body');
+    // }
+
+    /* **************************************************************************
+     * render                                                              */ /**
+     *
+     * Required method of a React component.
+     * @see {@link https://reactjs.org/docs/react-component.html#render|React.Component.render}
+     */
+    render() {
+        logger.debug(`DashboardView.render: only loading: ${this.props.numMeetingsToDisplay} Meetings`);
+        const meetingVisualizations = this.props.meetings.slice(0, this.props.numMeetingsToDisplay).map((m) => {
+            return (
+                <MeetingViz
+                    key={m._id}
+                    meeting={m}
+                    allMeetings={this.props.meetings}
+                    maybeLoadNextMeeting={this.props.maybeLoadNextMeeting}
+                    user={this.props.user}
+                />
+            );
+        });
+
+        const component = () => {
+            //errored (catch all static message is shown, these errors are all related to having no meetings)
+            if (this.props.loadingError.status) {
+                return (
+                    <LoadingErrorMessage>
+                        <div>{'Welcome to your Riff Dashboard!'}</div>
+                        <br/>
+                        <div>{'Once you have a Riff video meeting,'}</div>
+                        <div>{'your Riff stats will display here.'}</div>
+                    </LoadingErrorMessage>
+                );
+            }
+
+            //meetings
+            if (this.props.meetings.length > 0) {
+                return (
+                    <div style={{overflowY: 'scroll'}}>
+                        {meetingVisualizations}
+                    </div>
+                );
+            }
+
+            //default
+            return false;
+        };
+
+        return (
+            <div className='app__content'>
+                {component()}
+            </div>
+        );
+    }
+}
+
+/* **************************************************************************** *
+ * Module exports                                                               *
+ * **************************************************************************** */
+export {
+    DashboardView,
+};
--- components/dashboard/index.js
+++ components/dashboard/index.js
@@ -1,47 +1,27 @@
 // Copyright (c) 2018-present Riff Learning, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 
-import $ from 'jquery';
-import moment from 'moment';
-import lifecycle from 'react-pure-lifecycle';
 import {getCurrentUser} from 'mattermost-redux/selectors/entities/users';
 
 import {
     loadMoreMeetings,
     loadRecentMeetings,
-    selectMeeting,
-    loadMeetingData,
 } from 'actions/views/dashboard';
 import * as RiffServerActionCreators from 'actions/views/riff';
 import {logger} from 'utils/riff';
-import * as UserAgent from 'utils/user_agent.jsx';
 
-import DashboardView from './DashboardView';
+import {DashboardView} from './DashboardView';
 
 const mapStateToProps = (state) => {
-    logger.debug('Dashboard.mapStateToProps: state:', state);
-    const {lti, dashboard, riff} = state.views;
-    const riffState = {...lti, ...dashboard, ...riff};
     return {
         user: getCurrentUser(state),
-        riff,
-        riffAuthToken: riffState.authToken,
-        fetchMeetingsStatus: riffState.fetchMeetingsStatus,
-        fetchMeetingsMessage: riffState.fetchMeetingsMessage,
-        meetings: riffState.meetings,
-        numMeetings: riffState.numMeetings,
-        lastFetched: riffState.lastFetched,
-        shouldFetch: riffState.shouldFetch,
-        selectedMeeting: riffState.selectedMeeting || null,
-        processedUtterances: riffState.processedUtterances,
-        statsStatus: riffState.statsStatus,
-        processedNetwork: riffState.networkData,
-        processedTimeline: riffState.timelineData,
-        loadingError: riffState.loadingError,
-        numMeetingsToDisplay: riffState.numMeetingsToDisplay,
+        riffAuthToken: state.views.riff.authToken,
+        meetings: state.views.dashboard.meetings,
+        shouldFetch: state.views.dashboard.shouldFetch,
+        loadingError: state.views.dashboard.loadingError,
+        numMeetingsToDisplay: state.views.dashboard.numMeetingsToDisplay,
     };
 };
 
@@ -52,33 +32,11 @@ const mapDispatchToProps = (dispatch) => ({
     loadRecentMeetings: (uid) => {
         dispatch(loadRecentMeetings(uid));
     },
-    handleRefreshClick: (event, uid) => {
-        dispatch(loadRecentMeetings(uid));
-    },
-    handleMeetingClick: (event, uid, meeting) => {
-        event.preventDefault();
-
-        /// TODO: Why do we want 2 distinct actions to select the meeting and to load the meeting data? -mjl 2018-09-05
-        dispatch(selectMeeting(meeting));
-        dispatch(loadMeetingData(uid, meeting._id));
-    },
     authenticateRiff: () => {
         logger.debug('Dashboard.authenticateRiff: attempt data-server auth');
         dispatch(RiffServerActionCreators.attemptRiffAuthenticate());
     },
 });
-
-const formatMeetingDuration = (meeting) => {
-    if (!meeting) {
-        return '';
-    }
-
-    // support showing data on in progress meeting by giving them a fake end time of now
-    const startDate = new Date(meeting.startTime);
-    const endDate = meeting.endTime ? new Date(meeting.endTime) : new Date();
-    const diff = moment(endDate).diff(moment(startDate), 'minutes');
-    return `${diff} minutes`;
-};
 
 const mapMergeProps = (stateProps, dispatchProps, ownProps) => {
     logger.debug('Dashboard.mapMergeProps: user:', stateProps.user);
@@ -86,9 +44,6 @@ const mapMergeProps = (stateProps, dispatchProps, ownProps) => {
         ...stateProps,
         ...dispatchProps,
         ...ownProps,
-        selectedMeetingDuration: formatMeetingDuration(
-            stateProps.meetings[0]
-        ),
 
         // TODO: Load is the wrong terminalogy here, and maybe also in more of these properties as well.
         //       Consider renaming this to maybeDisplayNextMeeting -mjl 2019-04-26
@@ -109,48 +64,14 @@ const mapMergeProps = (stateProps, dispatchProps, ownProps) => {
     };
 };
 
-const componentDidUpdate = (props) => {
-    if (props.riffAuthToken && props.shouldFetch) {
-        props.loadRecentMeetings(props.user.id);
-    }
+const Dashboard = connect(
+    mapStateToProps,
+    mapDispatchToProps,
+    mapMergeProps)(DashboardView);
+
+/* **************************************************************************** *
+ * Module exports                                                               *
+ * **************************************************************************** */
+export {
+    Dashboard,
 };
-
-const componentDidMount = (props) => {
-    if (props.riffAuthToken) {
-        props.loadRecentMeetings(props.user.id);
-    }
-
-    $('body').addClass('app__body');
-
-    // IE Detection
-    if (UserAgent.isInternetExplorer() || UserAgent.isEdge()) {
-        $('body').addClass('browser--IE');
-    }
-};
-
-const componentWillMount = (props) => {
-    logger.debug('Dashboard.componentWillMount: user:', props.user);
-    if (!props.riff.authToken) {
-        props.authenticateRiff();
-    }
-};
-
-// componentWillUnmount = () => {
-//     $('body').removeClass('app__body');
-// }
-
-const methods = {
-    componentDidUpdate,
-    componentDidMount,
-    componentWillMount,
-};
-
-const Dashboard = lifecycle(methods)(DashboardView);
-
-export default withRouter(
-    connect(
-        mapStateToProps,
-        mapDispatchToProps,
-        mapMergeProps
-    )(Dashboard)
-);
