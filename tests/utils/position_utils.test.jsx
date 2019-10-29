092dc0dfcb8eea356bf8d6a3c48d99b6404f3a27
Mike Lippert <mike@rifflearning.com>
2019-10-29 18:21:10 -0400

    move last failing test suite to failing_tests directory, create test suite for test that wasn't in one
    The test summary is now:
    
    ```
    Test Suites: 216 passed, 216 total
    Tests:       11 skipped, 1327 passed, 1338 total
    Snapshots:   585 passed, 585 total
    Time:        16.787s
    Ran all test suites.
    ```

--- tests/utils/position_utils.test.jsx
+++ tests/utils/position_utils.test.jsx
@@ -2,14 +2,16 @@
 // See LICENSE.txt for license information.
 import {popOverOverlayPosition} from 'utils/position_utils.jsx';
 
-test('Should return placement position for overlay based on bounds, space required and innerHeight', function() {
-    const targetBounds = {
-        top: 400,
-        bottom: 500,
-    };
+describe('PositionUtils.popOverOverlayPosition', function() {
+    test('Should return placement position for overlay based on bounds, space required and innerHeight', function() {
+        const targetBounds = {
+            top: 400,
+            bottom: 500,
+        };
 
-    expect(popOverOverlayPosition(targetBounds, 1000, {above: 300})).toEqual('top');
-    expect(popOverOverlayPosition(targetBounds, 1000, {above: 500, below: 300})).toEqual('bottom');
-    expect(popOverOverlayPosition(targetBounds, 1000, {above: 450})).toEqual('bottom');
-    expect(popOverOverlayPosition(targetBounds, 1000, {above: 600})).toEqual('left');
+        expect(popOverOverlayPosition(targetBounds, 1000, {above: 300})).toEqual('top');
+        expect(popOverOverlayPosition(targetBounds, 1000, {above: 500, below: 300})).toEqual('bottom');
+        expect(popOverOverlayPosition(targetBounds, 1000, {above: 450})).toEqual('bottom');
+        expect(popOverOverlayPosition(targetBounds, 1000, {above: 600})).toEqual('left');
+    });
 });
