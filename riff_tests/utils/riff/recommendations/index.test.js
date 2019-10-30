1e5b2a554690015a60ea15d8454ade258eb9941b
Mike Lippert <mike@rifflearning.com>
2019-10-30 16:57:36 -0400

    jest wants 1 main describe per test file so I added one.
    If you have multiple top level describe calls, jest does funny things with the ones after the 1st making
    processing the test-results.xml file difficult.
    
    Also since jest considers each file a test suite, and doesn't put the file name in the test-results.xml file
    it makes sense that the top-level describe in each file be some form of the name of the file.

--- riff_tests/utils/riff/recommendations/index.test.js
+++ riff_tests/utils/riff/recommendations/index.test.js
@@ -26,93 +26,95 @@ const capstoneGroup = {
     channel_id: 'channel-capstone-test',
 };
 
-describe('getGroupChannel', () => {
-    const randomGroup = {
-        learning_group_prefix: 'useless',
-        members: [
-            {id: '1'},
-            {id: '2'},
-            {id: '3'},
-            {id: '4'},
-        ],
-        channel_id: 'unrelated-channel',
-    };
+describe('utils/riff/recommendations/index', () => {
+    describe('getGroupChannel', () => {
+        const randomGroup = {
+            learning_group_prefix: 'useless',
+            members: [
+                {id: '1'},
+                {id: '2'},
+                {id: '3'},
+                {id: '4'},
+            ],
+            channel_id: 'unrelated-channel',
+        };
 
-    const testGroups = [plgGroup, capstoneGroup, randomGroup];
+        const testGroups = [plgGroup, capstoneGroup, randomGroup];
 
-    it('should handle empty learningGroup being empty', () => {
-        expect(getGroupChannel('1', [], 'plg')).toEqual({});
-        expect(getGroupChannel('1', [], 'capstone')).toEqual({});
-    });
-
-    it('should return the requested learningGroup object based on prefix', () => {
-        expect(getGroupChannel('1', testGroups, 'plg').channelId).toEqual(plgGroup.channel_id);
-        expect(getGroupChannel('3', testGroups, 'plg').channelId).toEqual(plgGroup.channel_id);
-
-        expect(getGroupChannel('1', testGroups, 'capstone').channelId).toEqual(capstoneGroup.channel_id);
-        expect(getGroupChannel('7', testGroups, 'capstone').channelId).toEqual(capstoneGroup.channel_id);
-    });
-
-    it('should return an accurate array of teammateIds', () => {
-        let teammateIds = ['2', '3', '4'];
-        expect(getGroupChannel('1', testGroups, 'plg').teammateIds).toEqual(teammateIds);
-
-        teammateIds = ['1', '3', '4'];
-        expect(getGroupChannel('2', testGroups, 'plg').teammateIds).toEqual(teammateIds);
-
-        teammateIds = ['3', '5', '7'];
-        expect(getGroupChannel('1', testGroups, 'capstone').teammateIds).toEqual(teammateIds);
-
-        teammateIds = ['1', '5', '7'];
-        expect(getGroupChannel('3', testGroups, 'capstone').teammateIds).toEqual(teammateIds);
-    });
-});
-
-describe('getRecommendations', () => {
-    // have to mock these since we don't have access to the channel store
-    _test.getChannelURL = jest.fn();
-    _test.getChannelURL.mockReturnValue('url');
-
-    _test.getTownSquareId = jest.fn();
-    _test.getTownSquareId.mockReturnValue('town');
-
-    const millisecondsInAWeek = 604800000;
-
-    // we set our time offset to be in the middle of the week
-    // prevents any issues with time passing during the test
-    const timeOffset = millisecondsInAWeek / 2;
-
-    // mimic time passing by sending the course start time further
-    // back into the past
-    const getCourseStartTime = (week) => (Date.now() - timeOffset) - (millisecondsInAWeek * (week - 1));
-    const userId = '1';
-    const teamId = '2';
-    const learningGroups = [plgGroup, capstoneGroup];
-
-    // quick sanity check to make sure we're mocking time correctly
-    expect(getCurrentWeekNumber(getCourseStartTime(1))).toEqual(1);
-    expect(getCurrentWeekNumber(getCourseStartTime(3))).toEqual(3);
-
-    // generates list of numbers from 1 - 8
-    const weekNumbers = [...Array(8).keys()].map((x) => x + 1);
-    const recsByWeek = weekNumbers.map((week) => getRecommendations(userId, teamId, learningGroups, getCourseStartTime(week)));
-
-    // TODO - it is currently very difficult to test the isComplete functions since they all
-    // rely on the results of calls to external APIs
-    // for now we're just testing shouldDisplay
-    for (let i = 0; i < recsByWeek.length; i++) {
-        it(`should display the recommendations for week ${i} in that week`, () => {
-            for (const rec of recsByWeek[i]) {
-                const startWeek = rec.startWeek;
-                const endWeek = rec.startWeek + rec.numWeeksToDisplay;
-                const currentWeek = i + 1;
-                if (startWeek <= currentWeek && currentWeek < endWeek) {
-                    expect(rec.shouldDisplay()).toEqual(true);
-                }
-                else {
-                    expect(rec.shouldDisplay()).toEqual(false);
-                }
-            }
+        it('should handle empty learningGroup being empty', () => {
+            expect(getGroupChannel('1', [], 'plg')).toEqual({});
+            expect(getGroupChannel('1', [], 'capstone')).toEqual({});
         });
-    }
+
+        it('should return the requested learningGroup object based on prefix', () => {
+            expect(getGroupChannel('1', testGroups, 'plg').channelId).toEqual(plgGroup.channel_id);
+            expect(getGroupChannel('3', testGroups, 'plg').channelId).toEqual(plgGroup.channel_id);
+
+            expect(getGroupChannel('1', testGroups, 'capstone').channelId).toEqual(capstoneGroup.channel_id);
+            expect(getGroupChannel('7', testGroups, 'capstone').channelId).toEqual(capstoneGroup.channel_id);
+        });
+
+        it('should return an accurate array of teammateIds', () => {
+            let teammateIds = ['2', '3', '4'];
+            expect(getGroupChannel('1', testGroups, 'plg').teammateIds).toEqual(teammateIds);
+
+            teammateIds = ['1', '3', '4'];
+            expect(getGroupChannel('2', testGroups, 'plg').teammateIds).toEqual(teammateIds);
+
+            teammateIds = ['3', '5', '7'];
+            expect(getGroupChannel('1', testGroups, 'capstone').teammateIds).toEqual(teammateIds);
+
+            teammateIds = ['1', '5', '7'];
+            expect(getGroupChannel('3', testGroups, 'capstone').teammateIds).toEqual(teammateIds);
+        });
+    });
+
+    describe('getRecommendations', () => {
+        // have to mock these since we don't have access to the channel store
+        _test.getChannelURL = jest.fn();
+        _test.getChannelURL.mockReturnValue('url');
+
+        _test.getTownSquareId = jest.fn();
+        _test.getTownSquareId.mockReturnValue('town');
+
+        const millisecondsInAWeek = 604800000;
+
+        // we set our time offset to be in the middle of the week
+        // prevents any issues with time passing during the test
+        const timeOffset = millisecondsInAWeek / 2;
+
+        // mimic time passing by sending the course start time further
+        // back into the past
+        const getCourseStartTime = (week) => (Date.now() - timeOffset) - (millisecondsInAWeek * (week - 1));
+        const userId = '1';
+        const teamId = '2';
+        const learningGroups = [plgGroup, capstoneGroup];
+
+        // quick sanity check to make sure we're mocking time correctly
+        expect(getCurrentWeekNumber(getCourseStartTime(1))).toEqual(1);
+        expect(getCurrentWeekNumber(getCourseStartTime(3))).toEqual(3);
+
+        // generates list of numbers from 1 - 8
+        const weekNumbers = [...Array(8).keys()].map((x) => x + 1);
+        const recsByWeek = weekNumbers.map((week) => getRecommendations(userId, teamId, learningGroups, getCourseStartTime(week)));
+
+        // TODO - it is currently very difficult to test the isComplete functions since they all
+        // rely on the results of calls to external APIs
+        // for now we're just testing shouldDisplay
+        for (let i = 0; i < recsByWeek.length; i++) {
+            it(`should display the recommendations for week ${i} in that week`, () => {
+                for (const rec of recsByWeek[i]) {
+                    const startWeek = rec.startWeek;
+                    const endWeek = rec.startWeek + rec.numWeeksToDisplay;
+                    const currentWeek = i + 1;
+                    if (startWeek <= currentWeek && currentWeek < endWeek) {
+                        expect(rec.shouldDisplay()).toEqual(true);
+                    }
+                    else {
+                        expect(rec.shouldDisplay()).toEqual(false);
+                    }
+                }
+            });
+        }
+    });
 });
