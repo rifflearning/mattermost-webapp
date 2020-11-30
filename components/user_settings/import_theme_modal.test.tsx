87e334a992c3906ea2c5df971b3a968459e9e789
Mike Lippert <mike@riffanalytics.ai>
2020-11-30 15:28:19 -0500

    fix import slack theme test
    fix the test so it doesn't depend on the default theme values, AND so that it tests using values unlikely to already be used.

--- components/user_settings/import_theme_modal.test.tsx
+++ components/user_settings/import_theme_modal.test.tsx
@@ -4,6 +4,7 @@
 import React from 'react';
 
 import {Theme} from 'mattermost-redux/types/preferences';
+import {Preferences} from 'mattermost-redux/constants';
 
 import {mountWithIntl, shallowWithIntl} from 'tests/helpers/intl-test-helper';
 
@@ -16,35 +17,30 @@ describe('components/user_settings/ImportThemeModal', () => {
     });
 
     it('should correctly parse a Slack theme', () => {
-        const theme: Theme = {
-            sidebarBg: '#4d394b',
-            sidebarText: '#ffffff',
-            sidebarUnreadText: '#ffffff',
-            sidebarTextHoverBg: '#3e313c',
-            sidebarTextActiveBorder: '#4c9689',
-            sidebarTextActiveColor: '#ffffff',
-            sidebarHeaderBg: '#3e313c',
-            sidebarHeaderTextColor: '#ffffff',
-            onlineIndicator: '#38978d',
-            awayIndicator: '#ffbc42',
-            dndIndicator: '#f74343',
-            mentionBg: '#eb4d5c',
-            mentionBj: '#ffffff',
-            mentionColor: '#145dbf',
-            centerChannelBg: '#ffffff',
-            centerChannelColor: '#3d3c40',
-            newMessageSeparator: '#ff8800',
-            linkColor: '#2389d7',
-            buttonBg: '#166de0',
-            buttonColor: '#ffffff',
-            errorTextColor: '#fd5960',
-            mentionHighlightBg: '#ffe577',
-            mentionHighlightLink: '#166de0',
-            codeTheme: 'github',
-            type: 'custom',
-        };
+        const slackThemeProps = [
+            ['sidebarBg', '#010101'],
+            ['sidebarHeaderBg', '#020202'],
+            ['sidebarTextActiveBorder', '#030303'],
+            ['sidebarTextActiveColor', '#040404'],
+            ['sidebarTextHoverBg', '#050505'],
+            ['sidebarText', '#060606'],
+            ['onlineIndicator', '#070707'],
+            ['mentionBg', '#080808'],
+        ];
+        const slackTheme: Theme = slackThemeProps.reduce(
+            (theme, prop) => {
+                theme[prop[0]] = prop[1];
+                return theme;
+            },
+            {...Preferences.THEMES.default, type: 'custom'},
+        );
 
-        const themeString = '#4d394b,#3e313c,#4c9689,#ffffff,#3e313c,#ffffff,#38978d,#eb4d5c';
+        // the slack sidebarText also applies to 2 other MM theme props:
+        const sidebarText = slackThemeProps.find((p) => p[0] === 'sidebarText')?.[1] || '#01233210';
+        slackTheme.sidebarHeaderTextColor = sidebarText;
+        slackTheme.sidebarUnreadText = sidebarText;
+
+        const themeString = slackThemeProps.map((p) => p[1]).join(',');
         const wrapper = mountWithIntl(<ImportThemeModal/>);
         const instance = wrapper.instance();
 
@@ -57,6 +53,6 @@ describe('components/user_settings/ImportThemeModal', () => {
 
         wrapper.find('#submitButton').simulate('click');
 
-        expect(callback).toHaveBeenCalledWith(theme);
+        expect(callback).toHaveBeenCalledWith(slackTheme);
     });
 });
